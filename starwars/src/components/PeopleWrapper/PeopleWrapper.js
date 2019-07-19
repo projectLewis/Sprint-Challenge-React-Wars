320b277b0fdd6af5f0adbc8e5222c8fa23460adb
Seth Lewis <lewiscatenary@gmail.com>
2019-07-19 11:42:57 -0500

    replaced 'test text' with header

--- starwars/src/components/PeopleWrapper/PeopleWrapper.js
+++ starwars/src/components/PeopleWrapper/PeopleWrapper.js
@@ -1,11 +1,15 @@
 import React from "react";
 import People from "../People/People";
+import { Header, Icon } from "semantic-ui-react";
 
 const PeopleWrapper = ({ characters }) => {
   console.log(characters);
   return (
     <div>
-      Test1
+      <Header as="h1" icon textAlign="center">
+        <Icon name="space shuttle" inverted color={"red"} circular />
+        <Header.Content>Star Wars Top 10</Header.Content>
+      </Header>
       <People characters={characters} />
     </div>
   );
