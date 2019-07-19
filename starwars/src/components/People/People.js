74c34fa7fc286f94c3b7536a84f2c8eb9323fc0c
Seth Lewis <lewiscatenary@gmail.com>
2019-07-19 11:50:32 -0500

    removed fluid container from this component, moved to parent

--- starwars/src/components/People/People.js
+++ starwars/src/components/People/People.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, List, Segment } from "semantic-ui-react";
+import { List, Segment } from "semantic-ui-react";
 
 const People = ({ characters }) => {
   // console.log(characters);
@@ -7,19 +7,17 @@ const People = ({ characters }) => {
   return characters.map((character, idx) => {
     return (
       <>
-        <Container style={{ width: "300px", opacity: "0.7" }} fluid>
-          <Segment>
-            <List>
-              <List.Item>
-                <List.Header>{character.name}</List.Header>
-              </List.Item>
-              <List.Item>Gender: {character.gender}</List.Item>
-              <List.Item>Birth Year: {character.birth_year}</List.Item>
-              <List.Item>Height: {character.height}cm</List.Item>
-              <List.Item>Mass: {character.mass}</List.Item>
-            </List>
-          </Segment>
-        </Container>
+        <Segment>
+          <List>
+            <List.Item>
+              <List.Header>{character.name}</List.Header>
+            </List.Item>
+            <List.Item>Gender: {character.gender}</List.Item>
+            <List.Item>Birth Year: {character.birth_year}</List.Item>
+            <List.Item>Height: {character.height}cm</List.Item>
+            <List.Item>Mass: {character.mass}</List.Item>
+          </List>
+        </Segment>
       </>
     );
   });
