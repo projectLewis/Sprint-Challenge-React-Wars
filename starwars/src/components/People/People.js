8ae03bff9a2e047fde0b8dfba587b57bd598607e
Seth Lewis <lewiscatenary@gmail.com>
2019-07-19 11:32:26 -0500

    MVP complete: styling done

--- starwars/src/components/People/People.js
+++ starwars/src/components/People/People.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { Container, List, Segment } from "semantic-ui-react";
 
 const People = ({ characters }) => {
   // console.log(characters);
   // return <div>Test2</div>;
   return characters.map((character, idx) => {
     return (
-      <div id={idx} style={{ height: "30px", border: "3px solid blue" }}>
-        {character.name}
-      </div>
+      <>
+        <Container style={{ width: "300px", opacity: "0.7" }} fluid>
+          <Segment>
+            <List>
+              <List.Item>
+                <List.Header>{character.name}</List.Header>
+              </List.Item>
+              <List.Item>Gender: {character.gender}</List.Item>
+              <List.Item>Birth Year: {character.birth_year}</List.Item>
+              <List.Item>Height: {character.height}cm</List.Item>
+              <List.Item>Mass: {character.mass}</List.Item>
+            </List>
+          </Segment>
+        </Container>
+      </>
     );
   });
 };
